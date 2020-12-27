aa15bde7ba1ea17bcca219fe990babe30f6ab631
Adrimi <adriano100417@gmail.com>
2020-12-27 19:05:23 +0100

    Add javascript expression with the if-else condition pattern

--- 01-with-jsx/src/App.js
+++ 01-with-jsx/src/App.js
@@ -1,5 +1,15 @@
 import React from "react";
 
-const App = (props) => <div>01-with-jsx</div>;
+const Header = (props) => <h1>JSX Expression text</h1>;
+
+const agree = false;
+
+const App = (props) => {
+  if (agree) {
+    return <h1>Agree</h1>;
+  } else {
+    return <h1>Not Agree</h1>;
+  }
+};
 
 export default App;
