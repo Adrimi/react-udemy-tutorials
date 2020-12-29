3e8c3485ada629035de912343be0922303e7857e
Adrimi <adriano100417@gmail.com>
2020-12-29 18:45:26 +0100

    add click handler to stateful Class button

--- 02-props-events/src/ClassButton.js
+++ 02-props-events/src/ClassButton.js
@@ -5,8 +5,18 @@ class ClassButton extends Component {
     times: 5,
   };
 
+  handleClick = () => {
+    console.log("Clicked class/stateful component button");
+
+    this.setState({ times: this.state.times + 1 });
+  };
+
   render() {
-    return <button>Class Button, times: {this.state.times}</button>;
+    return (
+      <button onClick={this.handleClick}>
+        Class Button, times: {this.state.times}
+      </button>
+    );
   }
 }
 
