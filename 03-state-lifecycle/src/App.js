05958c387d46342f51a24bac052f01e771a671c1
Adrimi <adriano100417@gmail.com>
2020-12-30 16:22:07 +0100

    Create a stste

--- 03-state-lifecycle/src/App.js
+++ 03-state-lifecycle/src/App.js
@@ -1,7 +1,20 @@
+import React, { Component } from "react";
 import "./App.css";
 
-function App() {
-  return <div> Text :)</div>;
+class App extends Component {
+  state = {
+    count: 0,
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Current count: {this.state.count}</h1>
+        <button>Increase</button>
+        <button>Decrease</button>
+      </div>
+    );
+  }
 }
 
 export default App;
